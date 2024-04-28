0cae80df7fd17aaa36e8028f9eec924a10f02d66
RealNattawattHongthong <73651838+RealNattawattHongthong@users.noreply.github.com>
2024-04-29 01:06:22 +0700

    why

---
+++ src/data.ts
@@ -0,0 +1,4 @@
+export const fullVersionLink = 'https://resume.nattawatt.com';
+export const sourceLink = 'https://github.com/RealNattawattHongthong';
+export const ogImageUrl =
+	'https://nattawatt.com/assets/images/02.jpg';
